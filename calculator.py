307085d727cf8e186cc58c9f34308e9ea414bbed
Aman Sharma <amanpanditsharma07@gmail.com>
2025-07-13 15:51:04 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,18 @@
+a = int(input("Enter the number :"))
+b = int(input("Enter the number :"))
+
+
+sum = print(f"The sum of the numbers is : {a+b}")
+sub = print(f"The sub of the numbers is : {a-b}")
+multiply = print(f"The multiply of the numbers is : {a*b}")
+division = print(f"The division of the numbers is : {a/b}")
+floor_division = print(f"The floor division of the numbers is : {a//b}")
+power = print(f"The power of the a ^ b is : {a**b}")
+power = print(f"The power of the b ^ a is : {b**a}")
+
+
+
+## Time pass Quiz 
+# nums = list(range(1 , 11))
+# odd_cubes = [x**3 for x in nums if x%2 != 0 ]
+# print(odd_cubes)
